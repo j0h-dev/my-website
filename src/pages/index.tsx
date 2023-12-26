3a73089e781571b9d08499abbc9bc21c79dd8989
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2023-12-26 15:16:27 +0200

    Switched to the particle background on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-import profilePic from '@public/images/background.webp'
 import lavidaPreviewImage from '@public/images/lavida-website-preview.webp'
 
-import SkillBox from '@/components/skillBox'
-import { LinkButton } from '@/components/buttons'
+import SkillBox from '@/components/SkillBox'
+import { LinkButton } from '@/components/Buttons'
 import Head from 'next/head'
 import { Icon } from '@iconify/react'
+import Background from '@/components/Background'
 
 export default function Home() {
 	return (
@@ -18,13 +18,8 @@ export default function Home() {
 			</Head>
 
 			<div className="grid grid-cols-1 grid-rows-1 w-full justify-center items-center place-items-center min-h-[60vh]">
-				<Image
-					src={profilePic}
-					alt="Space background"
-					className="-z-10 object-cover max-h-[60vh] w-full h-full top-0 col-start-1 row-start-1"
-					sizes="(min-width: 768px) 70vw, (min-width: 1200px) 80vw, 100vw"
-					priority
-				/>
+				<Background className="-z-10 object-cover max-h-[60vh] w-full h-full top-0 col-start-1 row-start-1" />
+
 				<div className="flex flex-col w-4/5 gap-y-4 col-start-1 row-start-1">
 					<h1 className="flex flex-col gap-y-4 lg:text-7xl text-5xl text-slate-100">
 						<p>Hello world!</p>
@@ -41,12 +36,12 @@ export default function Home() {
 
 				<div className="flex flex-col gap-y-4 xl:w-3/5 w-4/5">
 					<p>
-						I'm a first-year student at Turku University of Applied Sciences. I'm studying Information and communications
-						technology. I'm eager to learn new things about my field and have a wide variety of skills under my belt.
+						I'm a first-year student at Turku University of Applied Sciences. I'm studying Information and communications technology. I'm
+						eager to learn new things about my field and have a wide variety of skills under my belt.
 					</p>
 					<p>
-						I've been interested in programming and technology in general since I was 10 years old. I started by learning
-						different things about computers, and after a while I learned my first programming language: Java.
+						I've been interested in programming and technology in general since I was 10 years old. I started by learning different things
+						about computers, and after a while I learned my first programming language: Java.
 					</p>
 					<p>I continued my learning journey by teaching myself new programming languages and technologies.</p>
 					<p>To this day, my journey still continues onward. </p>
@@ -151,13 +146,28 @@ export default function Home() {
 					<SkillBox>
 						<Icon icon="logos:stripe" fontSize="1.5em" />
 					</SkillBox>
+
+					<SkillBox>
+						<Icon icon="skill-icons:mongodb" fontSize="1.5em" />
+						<span>MongoDB</span>
+					</SkillBox>
+
+					<SkillBox>
+						<Icon icon="logos:figma" fontSize="1.5em" />
+						<span>Figma</span>
+					</SkillBox>
+
+					<SkillBox>
+						<Icon icon="skill-icons:tailwindcss-dark" fontSize="1.5em" />
+						<span>Tailwind</span>
+					</SkillBox>
 				</div>
 			</div>
 
 			<div className="xl:w-4/5 w-11/12 h-[2px] border-slate-100 border-dashed border-[1px]" />
 
 			<div className="flex flex-col items-center text-center gap-y-16 my-4 w-full">
-				<h2 className="text-3xl w-10/12">Some projects that I have done</h2>
+				<h2 className="text-3xl w-10/12">Some projects that I have worked in</h2>
 
 				<div className="flex xl:flex-row flex-col gap-x-14 gap-y-6 xl:w-3/5 w-4/5 items-center">
 					<Image src={lavidaPreviewImage} alt="Screenshot of liikuntapistelavida.com" width={320} className="rounded-xl" />
@@ -168,20 +178,21 @@ export default function Home() {
 						</div>
 
 						<p>
-							I was commisioned to create a website for a business named Liikuntapiste La Vida. It's a gym that offers
-							services like spa treatment, group exercise and other exercise related services.
+							I was commisioned to create a website for a business named Liikuntapiste La Vida. It's a gym that offers services like spa
+							treatment, group exercise and other exercise related services.
 						</p>
 						<p>
-							The website was created fully from scratch. The frontend has had multiple intereation in multiple different
-							JavaScript frameworks like Vue.js, Next.js, Svelte-kit and finally Nuxt.
+							The website was created fully from scratch. The frontend has had multiple intereation in multiple different JavaScript
+							frameworks like Vue.js, Next.js, Svelte-kit and finally Nuxt.
 						</p>
 						<p>The backend was created in TypeScript using the NestJS backend framework.</p>
 						<p>
-							While doing this project I learned multiple different frameworks (those previously mentioned), technologies and
-							other useful things. Here is a few things I learned:
+							While doing this project I learned multiple different frameworks (those previously mentioned), technologies and other useful
+							things. Here is a few things I learned:
 						</p>
 
 						<ul className="list-disc list-inside">
+							<li>Multiple frontend frameworks</li>
 							<li>Docker</li>
 							<li>MySQL</li>
 							<li>Server Management</li>
@@ -209,11 +220,7 @@ export default function Home() {
 				<h2 className="text-3xl w-10/12">Here are some places I visit sometimes</h2>
 
 				<div className="flex flex-wrap flex-row gap-3 w-3/5 items-center justify-center">
-					<Link
-						href="https://github.com/ItsOnlyGame"
-						target="_blank"
-						className="flex flex-row gap-x-2 justify-center items-center"
-					>
+					<Link href="https://github.com/ItsOnlyGame" target="_blank" className="flex flex-row gap-x-2 justify-center items-center">
 						<Icon icon="mdi:github" fontSize="2em" />
 						<p>@ItsOnlyGame</p>
 					</Link>
