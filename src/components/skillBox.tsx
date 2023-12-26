cf358f97c52eb79aaa7793ebb92b2b7da0808e45
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2023-12-26 15:14:27 +0200

    Changing components names to be capitalized

--- src/components/skillBox.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from 'react'
-
-export default function SkillBox({ children }: { children: ReactNode }) {
-	return <div className="flex flex-row gap-x-2 items-center justify-center p-2 rounded-xl bg-gray-700 text-gray-100">{ children }</div>
-}
