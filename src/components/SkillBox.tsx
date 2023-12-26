92f4b3030a9096e9297cf197c678464050942110
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2023-12-26 15:23:52 +0200

    Added a background hover color to SkillBox

--- src/components/SkillBox.tsx
+++ src/components/SkillBox.tsx
@@ -1,5 +1,9 @@
 import { ReactNode } from 'react'
 
 export default function SkillBox({ children }: { children: ReactNode }) {
-	return <div className="flex flex-row gap-x-2 items-center justify-center px-3 py-2 rounded-xl bg-gray-700 text-gray-100">{children}</div>
+	return (
+		<div className="flex flex-row gap-x-2 items-center justify-center px-3 py-2 rounded-xl bg-gray-700 hover:bg-gray-600 text-gray-100">
+			{children}
+		</div>
+	)
 }
