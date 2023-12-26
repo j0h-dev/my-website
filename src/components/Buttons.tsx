903cbbf06c1137b8037a1bfe04c8f129ddec4186
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2023-12-26 15:23:37 +0200

    Moved scss to component as tailwindcss

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,11 +1,10 @@
 import Link from 'next/link'
 import { ReactNode } from 'react'
-import style from '@/styles/buttons.module.scss'
 
 export function LinkButton({ children, href, blank }: { children: ReactNode; href: string; blank: boolean }) {
 	return (
 		<Link href={href} target={blank ? '_blank' : '_self'}>
-			<button className={style['link-button']}>{children}</button>
+			<button className="px-3 py-2 bg-gray-700 hover:bg-gray-600 text-gray-100 rounded-md transition-colors">{children}</button>
 		</Link>
 	)
 }
