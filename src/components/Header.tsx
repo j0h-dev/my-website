2bea652d57be1c00e72b68e706ba285e18023932
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2023-12-27 07:28:38 +0200

    Set user-select to none in Header site component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
 			<Background className="-z-10 object-cover max-h-[60vh] w-full h-full top-0 col-start-1 row-start-1" />
 
 			<div className="flex flex-col w-4/5 gap-y-4 col-start-1 row-start-1">
-				<h1 className="flex flex-col gap-y-4 lg:text-7xl text-5xl text-slate-100 font-semibold">
+				<h1 className="flex flex-col gap-y-4 lg:text-7xl text-5xl text-slate-100 font-semibold select-none">
 					<p>Hello world!</p>
 					<p>
 						My name is <span className="text-[#006cc6]">Jaro</span>
