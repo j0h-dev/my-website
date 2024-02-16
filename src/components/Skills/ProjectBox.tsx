dd9647b8e40cf09a6ed32c2f7c63fcec24b44fc5
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2024-02-16 11:10:32 +0200

    Update ProjectBox styling

--- src/components/Skills/ProjectBox.tsx
+++ src/components/Skills/ProjectBox.tsx
@@ -1,3 +1,3 @@
 export default function ProjectBox({ children }: React.PropsWithChildren) {
-	return <div className="flex md:flex-row flex-col gap-x-14 gap-y-6 items-center py-20 first:pt-8 px-2">{children}</div>
+	return <div className="flex xl:flex-row flex-col gap-x-14 gap-y-6 items-center py-20 first:pt-8 px-2">{children}</div>
 }
