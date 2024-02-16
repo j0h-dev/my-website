f1bf2ae866123ae224050d9dc10f5bb265b8ce02
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2024-02-16 11:04:11 +0200

    Update content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,20 +13,26 @@ export default function Home() {
 			</Head>
 
 			<div className="flex flex-col items-center text-center gap-y-6 mt-8 w-full">
-				<h2 className="text-3xl w-full">About me</h2>
+				<h2 className="text-3xl w-full">My Journey</h2>
 
 				<div className="flex flex-col gap-y-4 w-full text-left">
 					<p>
-						I'm a second-year student at Turku University of Applied Sciences. I'm studying Information and communications technology. I'm
-						eager to learn new things about my field and have a wide variety of skills under my belt.
+						I'm a second-year student at Turku University of Applied Sciences. I'm studying Information and communications technology. I
+						like to game, hang out with friends, and program weird and useful things.
 					</p>
 					<p>
-						I've always been interested in computers and technology, which led me to become computer literate. I started learning how to
-						program at the age of 10, with Java being my first language.
+						After I got my first laptop at the age of 10, I started learning how to program with Java being my first language. I got started
+						by watching YouTube tutorials. After I felt confident, I began to create my own game, but after a while my interest shifted to
+						learning different programming languages.
 					</p>
 					<p>
-						Now I have taken more of an interest in web development. I've mostly been playing around as a fullstack developer with Nuxt and
-						React in the frontend and NestJS and Express.js in the backend.
+						After that, I began to pick up more languages like Python, JavaScript and C++ and created some small projects with them. During
+						2020, I got the opportunity put my skills into use by creating a website for a client. At this point the ball of web
+						developement started rolling.
+					</p>
+					<p>
+						Now I've mostly been playing around as a fullstack developer with Nuxt or React in the frontend and NestJS or Express.js in the
+						backend.
 					</p>
 					<p>Even today, I am still learning more and more. One day I will become a computer wizard🔮</p>
 				</div>
