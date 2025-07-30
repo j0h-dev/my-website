0c8fde528ae620a895af40a6a47476fd88f41681
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2025-07-30 22:23:09 +0300

    chore: update routeTree.gen.ts

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -8,26 +8,42 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-// Import Routes
+import { Route as rootRouteImport } from './routes/__root'
+import { Route as DefaultRouteImport } from './routes/_default'
+import { Route as DefaultIndexRouteImport } from './routes/_default/index'
 
-import { Route as rootRoute } from './routes/__root'
-import { Route as DefaultImport } from './routes/_default'
-import { Route as DefaultIndexImport } from './routes/_default/index'
-
-// Create/Update Routes
-
-const DefaultRoute = DefaultImport.update({
+const DefaultRoute = DefaultRouteImport.update({
   id: '/_default',
-  getParentRoute: () => rootRoute,
+  getParentRoute: () => rootRouteImport,
 } as any)
-
-const DefaultIndexRoute = DefaultIndexImport.update({
+const DefaultIndexRoute = DefaultIndexRouteImport.update({
   id: '/',
   path: '/',
   getParentRoute: () => DefaultRoute,
 } as any)
 
-// Populate the FileRoutesByPath interface
+export interface FileRoutesByFullPath {
+  '/': typeof DefaultIndexRoute
+}
+export interface FileRoutesByTo {
+  '/': typeof DefaultIndexRoute
+}
+export interface FileRoutesById {
+  __root__: typeof rootRouteImport
+  '/_default': typeof DefaultRouteWithChildren
+  '/_default/': typeof DefaultIndexRoute
+}
+export interface FileRouteTypes {
+  fileRoutesByFullPath: FileRoutesByFullPath
+  fullPaths: '/'
+  fileRoutesByTo: FileRoutesByTo
+  to: '/'
+  id: '__root__' | '/_default' | '/_default/'
+  fileRoutesById: FileRoutesById
+}
+export interface RootRouteChildren {
+  DefaultRoute: typeof DefaultRouteWithChildren
+}
 
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
@@ -35,20 +51,18 @@ declare module '@tanstack/react-router' {
       id: '/_default'
       path: ''
       fullPath: ''
-      preLoaderRoute: typeof DefaultImport
-      parentRoute: typeof rootRoute
+      preLoaderRoute: typeof DefaultRouteImport
+      parentRoute: typeof rootRouteImport
     }
     '/_default/': {
       id: '/_default/'
       path: '/'
       fullPath: '/'
-      preLoaderRoute: typeof DefaultIndexImport
-      parentRoute: typeof DefaultImport
+      preLoaderRoute: typeof DefaultIndexRouteImport
+      parentRoute: typeof DefaultRoute
     }
   }
 }
-
-// Create and export the route tree
 
 interface DefaultRouteChildren {
   DefaultIndexRoute: typeof DefaultIndexRoute
@@ -61,61 +75,9 @@ const DefaultRouteChildren: DefaultRouteChildren = {
 const DefaultRouteWithChildren =
   DefaultRoute._addFileChildren(DefaultRouteChildren)
 
-export interface FileRoutesByFullPath {
-  '': typeof DefaultRouteWithChildren
-  '/': typeof DefaultIndexRoute
-}
-
-export interface FileRoutesByTo {
-  '/': typeof DefaultIndexRoute
-}
-
-export interface FileRoutesById {
-  __root__: typeof rootRoute
-  '/_default': typeof DefaultRouteWithChildren
-  '/_default/': typeof DefaultIndexRoute
-}
-
-export interface FileRouteTypes {
-  fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '' | '/'
-  fileRoutesByTo: FileRoutesByTo
-  to: '/'
-  id: '__root__' | '/_default' | '/_default/'
-  fileRoutesById: FileRoutesById
-}
-
-export interface RootRouteChildren {
-  DefaultRoute: typeof DefaultRouteWithChildren
-}
-
 const rootRouteChildren: RootRouteChildren = {
   DefaultRoute: DefaultRouteWithChildren,
 }
-
-export const routeTree = rootRoute
+export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
   ._addFileTypes<FileRouteTypes>()
-
-/* ROUTE_MANIFEST_START
-{
-  "routes": {
-    "__root__": {
-      "filePath": "__root.tsx",
-      "children": [
-        "/_default"
-      ]
-    },
-    "/_default": {
-      "filePath": "_default.tsx",
-      "children": [
-        "/_default/"
-      ]
-    },
-    "/_default/": {
-      "filePath": "_default/index.tsx",
-      "parent": "/_default"
-    }
-  }
-}
-ROUTE_MANIFEST_END */
