8a09b2ce05bb86fea66934f09597fdf68cba38c0
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2025-07-31 02:43:49 +0300

    feat: remove tanstack router devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,18 +1,14 @@
 import { ThemeProvider } from '@/components/theme-provider'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRoute({
   component: RootComponent,
 })
 
-const isDevelopmentMode = import.meta.env.DEV
-
 function RootComponent() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="j0h-theme">
       <Outlet />
-      {isDevelopmentMode && <TanStackRouterDevtools position="bottom-right" />}
     </ThemeProvider>
   )
 }
