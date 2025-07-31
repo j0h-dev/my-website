82e79046c6c89fbdc374dc4bb0ec5745ecbf9c4f
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2025-07-31 18:23:56 +0300

    feat: add backdrop blur to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="mt-12 flex h-24 flex-col items-center justify-center gap-4 border-t py-8">
+    <footer className="mt-12 flex h-24 flex-col items-center justify-center gap-4 border-t py-8 backdrop-blur-sm">
       <p>@Made by Jaro Heinonen</p>
     </footer>
   )
