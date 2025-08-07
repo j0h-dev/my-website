e5fca868869150576a46a0538fc6f4533edcb351
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2025-08-07 03:36:21 +0300

    build: setup rollup manual chunking

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
     }),
     react(),
     tailwindcss(),
+    visualizer(),
   ],
   resolve: {
     alias: {
@@ -20,4 +22,33 @@ export default defineConfig({
     },
   },
   publicDir: './public',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes('tanstack')) {
+            return '@tanstack'
+          }
+
+          if (id.includes('tsparticles')) {
+            return '@tsparticles'
+          }
+
+          if (id.includes('lucide') || id.includes('icons')) {
+            return '@vendor'
+          }
+
+          if (id.includes('radix')) {
+            return '@vendor'
+          }
+
+          if (id.includes('react')) {
+            return '@react'
+          }
+
+          return '@vendor'
+        },
+      },
+    },
+  },
 })
