237f7abf4a895499e0240f2f9e9dcde7d5cb6e31
Jaro Heinonen <heinonenjaro@proton.me>
2025-11-30 15:46:22 +0200

    feat: update experiences

--- src/components/content/experiences.tsx
+++ src/components/content/experiences.tsx
@@ -1,4 +1,4 @@
-import type { Experience } from '@/types/experience'
+import type { Experience } from '@/types'
 import { ExperienceItem } from './experience-item'
 
 const experiences: Experience[] = [
@@ -7,12 +7,15 @@ const experiences: Experience[] = [
     company: 'Labriikki - TurkuAMK',
     period: 'May 2025 - September 2025',
     description: [
-      'Lead full-stack development for a web application to help TurkuAMK laboratories with 6S Lean principles, taking ownership of both frontend and backend work. I designed backend architectures and database schemas, ensuring scalable and maintainable systems.',
+      'This role began as a capstone project and turned into an internship, where I stepped into a Lead Developer position. I led full-stack development using React for the frontend and Node.js with Express and MySQL on the backend, contributing to both core features and overall application architecture.',
+      'In addition to hands-on development, I provided technical leadership through code reviews, architectural guidance, and mentoring, ensuring the usage of best practices and maintaining high code quality across the team.',
+      'I designed and implemented automated CI/CD pipelines with GitLab CI to streamline testing and deployments, significantly improving development efficiency and release reliability.',
     ],
     technologies: [
       'React',
       'TypeScript',
       'Node.js',
+      'Express.js',
       'MySQL',
       'Tailwind CSS',
       'GitLab CI/CD',
@@ -25,6 +28,7 @@ const experiences: Experience[] = [
     period: 'May 2020 - August 2025',
     description: [
       'Build a modern business website for a gym, featuring user authentication, admin dashboard, and a reservation calendar. I also maintain the website and ensure its smooth operation.',
+      'I used this freelance job as a learning experience to learn multiple different technologies that I was interested in.',
     ],
     technologies: [
       'Nuxt.js',
@@ -45,7 +49,7 @@ const experiences: Experience[] = [
     ],
   },
   {
-    title: 'Lead Full-Stack Developer & Tech Lead',
+    title: 'Lead Full-Stack Developer',
     company: 'theFIRMA - TurkuAMK',
     period: 'May 2024 - December 2024',
     description: [
@@ -59,6 +63,7 @@ const experiences: Experience[] = [
       'React',
       'TypeScript',
       'Node.js',
+      'Express.js',
       'MySQL',
       'Postgres',
       'Tailwind CSS',
