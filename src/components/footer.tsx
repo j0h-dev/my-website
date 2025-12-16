6f93e8786fb9e148b02942e1d51a9ac1a0871777
Jaro Heinonen <heinonenjaro@proton.me>
2025-12-16 04:59:42 +0200

    feat: update footer height and text font

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="mt-12 flex h-24 flex-col items-center justify-center gap-4 border-t py-8 backdrop-blur-sm">
+    <footer className="mt-6 flex h-8 flex-col items-center justify-center gap-1 border-t py-8 backdrop-blur-sm text-muted-foreground text-sm">
       <p>@Made by Jaro Heinonen</p>
     </footer>
   )
